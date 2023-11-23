2db4fbcf4c58cbaa744e3bea750f98bf810441d5
Khang0608 <hoduykhanghocmai@gmail.com>
2023-11-23 10:31:18 +0700

    star rating done

--- Frontend-Folder-Structure/src/components/index.js
+++ Frontend-Folder-Structure/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Button } from "./Button";
 export { default as Inputs } from "./Inputs";
 export { default as Modal } from "./Modal";
 export { default as ToolTip } from "./ToolTip";
+export {default as StarRating} from "./StarRating/star"
