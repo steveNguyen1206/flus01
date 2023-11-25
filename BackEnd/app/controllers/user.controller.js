83fe59057184f2b995490b3a29c6074fb58fc9e3
NTTVy03 <trucvy05062003@gmail.com>
2023-11-25 09:19:37 +0700

    API user

---
+++ BackEnd/app/controllers/user.controller.js
@@ -0,0 +1,23 @@
+const db = require("../models");
+const User = db.user;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new User
+exports.create = (req, res) => {
+
+};
+
+// Retrieve all User from the database - search name
+exports.findAll = (req, res) => {
+  
+};
+
+// Find a single User with an id
+exports.findOne = (req, res) => {
+  
+};
+
+// Update a User by the id in the request
+exports.update = (req, res) => {
+  
+};
