f2309a7009271e66b87e9ef325ac5cf6245898cf
DatLe <lethanhdat0246@gmail.com>
2023-12-09 10:10:32 +0700

    connect signup

--- Frontend-Folder-Structure/src/components/SignUpTabs/signup_tab_third.jsx
+++ Frontend-Folder-Structure/src/components/SignUpTabs/signup_tab_third.jsx
@@ -1,25 +1,70 @@
-import React from "react";
-import './signup_tab_third.css'
+import React from 'react';
+import './signup_tab_third.css';
+import authServices from '@/services/authServices'
+const SignUpTabThird = ({ setTab, signUpPayload, setSignUpPayload, onSignUp}) => {
 
-const signUpTabThird = () => {
-    return(
-      <div className='info-field'>
-        <div className="input-container">
-          <label for="inputUsername" class="form-label">CODE</label>
-          <input id="inputUsername" type="text" class="form-control" placeholder="Enter code we send to your phone number..." aria-label="Username" aria-describedby="basic-addon1"/>
-        </div>
-        
+  const signin = () => {
+    var data = {
+      account_name: signUpPayload.userName,
+      password: signUpPayload.userPassword,
+      profile_name: signUpPayload.realName,
+      phone_number: signUpPayload.phone,
+      nationality: signUpPayload.country,
+      user_type: 1,
+      email: signUpPayload.email,
+      avt_url: "https://imgur.com/gallery/ApNKGxs",
+      social_link: "https://imgur.com/gallery/ApNKGxs",
+    };
 
-        <div className="sign-up-button row">
-          <div className="div-wrapper-b">
-            <div className="text-wrapper-2">Back</div>
-          </div>
-          <div className="div-wrapper-n ">
-            <div className="text-wrapper-2-n">Enter</div>
-          </div>
-        </div>
+    // console.log(data)
+    authServices.signup(data)
+      .then(response => {
+        if(response.status == 200)
+        {
+          console.log("Sign up successfully")
+          
+        }
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+
+  const handleEnterClick = () => {
+    signin();
+    onSignUp();
+  };
+
+  return (
+    <div className="info-field">
+      <div className="input-container">
+        <label htmlFor="inputUsername" className="form-label">
+          CODE
+        </label>
+        <input
+          id="inputUsername"
+          type="text"
+          name='code'
+          className="form-control"
+          placeholder="Enter code we send to your phone number..."
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
       </div>
-    )
-}
 
-export default signUpTabThird;
+      <button onClick={() => setTab(2)} className="sign-up-button row">
+        <div className="div-wrapper-b">
+          <div className="text-wrapper-2">Back</div>
+        </div>
+      </button>
+      <button onClick={() => {handleEnterClick()}} className="sign-up-button row">
+        <div className="div-wrapper-n">
+          <div className="text-wrapper-2-n">Enter</div>
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default SignUpTabThird;
--- Frontend-Folder-Structure/src/pages/index.js
+++ Frontend-Folder-Structure/src/pages/index.js
@@ -4,5 +4,6 @@ export { PayPalDemo, PayPalPayment } from "./Payment";
 export { default as Shop } from "./Shop";
 export {default as SignUp} from "./SignUp/signUp";
 export { default as LogIn } from "./Login/login";
-export { default as Login2 } from "./Login/login2";
+export { default as Project } from "./Project/project";
+// export { default as Login2 } from "./Login/login2";
 export {default as Profile} from "./Profile/profile"
