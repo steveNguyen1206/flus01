4184496c5206a226b533f4ea56b0a21684b612c0
Khang0608 <hoduykhanghocmai@gmail.com>
2023-12-11 13:40:53 +0700

    add .env to frontend for google client id

--- Frontend-Folder-Structure/src/Routers/Routers.jsx
+++ Frontend-Folder-Structure/src/Routers/Routers.jsx
@@ -4,10 +4,11 @@ import { Footer, Header, Navbar } from '@/layout';
 import { PayPalDemo } from '@/pages/Payment';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Login2 } from '@/pages';
+const clientId = process.env.GG_CLIENT_ID;
 
 const Routers = () => {
   return (
-    <GoogleOAuthProvider clientId="138372560551-k6qucf4eebnppht116rieqoa6bfm801b.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId= {clientId}>
       <Router>
         <Header />
         <Routes>
